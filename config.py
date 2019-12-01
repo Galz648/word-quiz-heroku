f8f5c3b8e923cec2bced85314a151a6ad36631f4
FachFachFach <theycallmefachfach@gmail.com>
2019-12-01 21:46:18 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,2 @@
+# variable setup
+wordnikApiKey = "hoh3cbniehkwxjk0b93leoxieddrvll7cr63ay4sjvasj6eh6"
---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from config import wordnikApiKey
+from os import environ
+
+environ["FLASK_APP"] = __name__
+environ["FLASK_DEBUG"] = "1"
+app = Flask(__name__)
+
+
+@app.route('/api/word/define')
+def define():
+    
+if __name__ == "__main__":
+    app.run()
