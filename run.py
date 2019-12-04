e83751c62c445d861381249f68aedaa4ec7789fd
FachFachFach <theycallmefachfach@gmail.com>
2019-12-04 17:37:02 +0200

    api commit

--- run.py
+++ run.py
@@ -9,28 +9,26 @@ wordApi = WordApi.WordApi(client)
 environ["FLASK_APP"] = __name__
 environ["FLASK_DEBUG"] = "1"
 app = Flask(__name__)
-app.DEBUG = True
 
 @app.route('/api/word/define')
 def define():
     try:
         word = request.args['word']
         print('started')
-        definition = wordApi.getDefinitions(word=word, limit=1)[0]
+        definition = wordApi.getDefinitions(word=word, limit=1)[0].text
         
     except Exception as e:
         abort(404)
     else:
-        jsonify(definition=definition) 
+        return jsonify(data=definition)
     
 
 @app.route('/api/word/related')
 def related():
     word = request.args['word']
     related = wordApi.getRelatedWords(word=word)[0].words
-    print(related.words)
     print(dir(related))
-    return jsonify(related=)
+    return jsonify(data=related)
     
     
     
@@ -40,7 +38,7 @@ def sentences():
     word = request.args['word']
     examples_list = [example.text for example in wordApi.getExamples(word=word, limit=limit).examples]
     print(f'examples_list: {examples_list}')
-    return jsonify(sentences=examples_list)
+    return jsonify(data=examples_list)
 
 
 if __name__ == "__main__":
