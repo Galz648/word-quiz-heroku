e126e25c2450f76231fabca697b9956e4a543d11
FachFachFach <theycallmefachfach@gmail.com>
2020-09-19 01:01:56 +0300

    added missing package wordnik; return exception on error

--- run.py
+++ run.py
@@ -10,35 +10,50 @@ environ["FLASK_APP"] = __name__
 environ["FLASK_DEBUG"] = "1"
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "<p> /api/word/define?word='{word}' <br> /api/word/related?word='{word}' <br> /api/word/sentences?word='{word}' </p>"
+
+# Example : /api/word/define?word='{word}'
 @app.route('/api/word/define')
 def define():
     try:
+        limit = 1
         word = request.args['word']
         print('started')
-        definition = wordApi.getDefinitions(word=word, limit=1)[0].text
-        
-    except Exception as e:
-        abort(404)
-    else:
+        definition = wordApi.getDefinitions(word=word, limit=limit)[0].text
         return jsonify({'data':definition})
-    
 
+    except Exception as e:
+        jsonify({'error':e})
+        abort(404)
+
+       
+# Example : /api/word/related?word='{word}'
 @app.route('/api/word/related')
 def related():
-    word = request.args['word']
-    related = wordApi.getRelatedWords(word=word)[0].words
-    print(dir(related))
-    return jsonify({'data':related})
+    try:
+        limit = 4
+        word = request.args['word']
+        related = wordApi.getRelatedWords(word=word, limit=limit)[0].words
+        print(dir(related))
+        return jsonify({'data':related})
+    except Exception as e:
+        jsonify({'error':e})
+        abort(404)
     
-    
-    
+# Example : /api/word/sentences?word='{word}'
 @app.route('/api/word/sentences')
 def sentences():
-    limit = 3
-    word = request.args['word']
-    examples_list = [example.text for example in wordApi.getExamples(word=word, limit=limit).examples]
-    print(f'examples_list: {examples_list}')
-    return jsonify({'data':examples_list})
+    try:
+        limit = 3
+        word = request.args['word']
+        examples_list = [example.text for example in wordApi.getExamples(word=word, limit=limit).examples]
+        print(f'examples_list: {examples_list}')
+        return jsonify({'data':examples_list})
+    except Exception as e:
+        jsonify({'error':e})
+        abort(404)
 
 
 if __name__ == "__main__":
