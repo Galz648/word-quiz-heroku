2f1f712de1b3808b9268e9a4446b318fe04d0297
FachFachFach <theycallmefachfach@gmail.com>
2020-09-19 02:03:54 +0300

    cors fix

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, abort
 from config import wordnikApiKey
 from os import environ
+from flask_cors import CORS
+
 
 from wordnik import *
 apiUrl = 'http://api.wordnik.com/v4'
@@ -9,6 +11,7 @@ wordApi = WordApi.WordApi(client)
 environ["FLASK_APP"] = __name__
 environ["FLASK_DEBUG"] = "1"
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
 def home():
